2b1d4dae6096e81c19a316faa2bac472db115db5
Augusto <asergioscosta@gmail.com>
2023-12-22 12:28:22 -0300

    Estilizando a página index

--- trupencanta/aplic/urls.py
+++ trupencanta/aplic/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, SaibaMaisView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-
-]
+    path('saiba-mais/', SaibaMaisView.as_view(), name='saiba-mais'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
